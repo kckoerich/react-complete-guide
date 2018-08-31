be4d1ac3c64011110e1cbb48afd0b97a571a442b
kckoerich <kaio@geomais.com.br>
2018-08-31 11:12:43 -0300

    section 5, lecture 65

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import './Person.css';
+import classes from './Person.css';
 //import Radium from 'radium'
 
 const person = (props) => {
@@ -7,7 +7,7 @@ const person = (props) => {
    
 
     return (
-        <div className='Person' /*style={style}*/>
+        <div className={classes.Person} /*style={style}*/>
             <p onClick={props.click}>I'm {props.name} and I am {props.age} years old!</p>
             <p>{props.children}</p>
             <input type="text" onChange={props.changed} value={props.name}/>
